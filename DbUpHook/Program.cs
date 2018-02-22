3fe5c474619405e660756536509eedf711108f0f
Amrit Purshotam <amrit.purshotam@gmail.com>
2018-02-22 20:14:17 +0200

    Add initial dbup duplicate script number checker

---
+++ DbUpHook/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace DbUpHook
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var xml = new XmlDocument();
+            xml.Load(Settings.Path);
+            var contents = xml.GetElementsByTagName("Content");
+
+            List<int> numbers = new List<int>();
+            foreach (XmlNode content in contents)
+            {
+                if (content.Attributes != null)
+                {
+                    var include = content.Attributes["Include"];
+                    if (include != null)
+                    {
+                        var value = include.Value;
+                        if (value.StartsWith("Scripts") && value.EndsWith(".sql"))
+                        {
+                            var filename = include.Value.Split('\\').Last();
+                            var numberString = filename.Split('-').FirstOrDefault();
+                            if (numberString != null)
+                            {
+                                var success = Int32.TryParse(numberString, out var number);
+                                if (success)
+                                {
+                                    if (numbers.Contains(number) && !Settings.Ignore.Contains(number))
+                                    {
+                                        return number;
+                                    }
+                                    numbers.Add(number);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ DbUpHook/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace DbUpHook
+{
+    public static class Settings
+    {
+        public static IList<int> Ignore = 
+            string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["Ignore"])
+                ? new List<int>()
+                : ConfigurationManager.AppSettings["Ignore"].Split(',').Select(int.Parse).ToList();
+
+        public static string Path = ConfigurationManager.AppSettings["Path"];
+    }
+}
